b21d0509f81edbb0aedf0c0ce1d261a02ec8deca
innocentmaweta-hub <innocentmaweta@gmail.com>
2026-07-26 14:36:50 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './index.css';
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={clientId}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
